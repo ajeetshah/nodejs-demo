3f6a3cb65ddae1d3f3299fb37afd2ee6e0b9a1f6
Ajeet Shah <ajeets.shah@hotmail.com>
2020-11-05 22:48:27 +0530

    Add 404 route

--- src/hello/helloModule.js
+++ src/hello/helloModule.js
@@ -6,8 +6,16 @@ const port = 8083;
 
 http
   .createServer((req, res) => {
-    res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end(`${myUtils.hello}, ${myUtils.now()}`);
+    if (req.url === "/") {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end(myUtils.hello);
+    } else if (req.url === "/v1/api/now") {
+      res.writeHead(200, { "Content-Type": "text/plain" });
+      res.end(myUtils.now());
+    } else {
+      res.writeHead(404, { "Content-Type": "text/plain" });
+      res.end("Not Found!");
+    }
   })
   .listen(port, hostname, () => {
     myUtils.printServerInfo(port);
